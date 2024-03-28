a71466b5a1257c747edf3eccd97d9c73c8aed90d
Rebecca <rebecca@silentweb.nz>
2024-03-28 15:23:38 +1300

    1.8.1

--- src/register.js
+++ src/register.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const Router = require('@koa/router')
-const { createIPX } = require('ipx')
+const { createIPX, ipxFSStorage } = require('ipx')
 const { resolve } = require('path')
 const { existsSync, mkdirSync } = require('fs')
 const { createMiddleware } = require('./middleware')
@@ -10,12 +10,8 @@ function register({ strapi }) {
   const config = strapi.config.get('plugin.local-image-sharp')
   config.srcDir = strapi.dirs?.static?.public ?? strapi.dirs?.public
 
-  strapi.log.info(
-    `Using Local Image Sharp plugin`
-  );
-  strapi.log.info(
-    `- Source directory: ${config.srcDir}`
-  );
+  strapi.log.info(`Using Local Image Sharp plugin`)
+  strapi.log.info(`- Source directory: ${config.srcDir}`)
 
   if (config.cacheDir) {
     const cwd = process.cwd()
@@ -31,15 +27,15 @@ function register({ strapi }) {
       mkdirSync(config.cacheDir, { recursive: true })
     }
 
-    strapi.log.info(
-      `- Cache directory: ${config.cacheDir}`
-    );
+    strapi.log.info(`- Cache directory: ${config.cacheDir}`)
   }
 
   const router = new Router()
-  config.paths.forEach(path => {
+
+  config.paths.forEach((path) => {
     const ipx = createIPX({
       dir: config.srcDir + path,
+      storage: ipxFSStorage({ dir: config.srcDir + path })
     })
 
     router.get(`${path}/(.*)`, createMiddleware(ipx))
@@ -49,5 +45,5 @@ function register({ strapi }) {
 }
 
 module.exports = {
-  register,
+  register
 }
